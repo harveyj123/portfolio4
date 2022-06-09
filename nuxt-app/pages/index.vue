9bca87df4433a984f551b3cdb225f804d3d916fd
harveyj123 <71397990+harveyj123@users.noreply.github.com>
2022-06-09 15:49:36 -0400

    some body content

---
+++ nuxt-app/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+ <div>
+  <Nav></Nav>
+  <div>
+   <h1>Hi im Harvey</h1>
+   <h2>A fullstack developer</h2>
+  </div>
+ </div>
+</template>
+
+<script setup>
+// import "./assets/css/tailwind.css";
+</script>
