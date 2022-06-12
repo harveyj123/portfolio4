f6e05ee65619b0ca8ec20142f8216d3d7fedce7c
harveyj123 <71397990+harveyj123@users.noreply.github.com>
2022-06-12 14:50:04 -0400

    new nav

---
+++ nuxt-app/components/Projects.vue
@@ -0,0 +1,28 @@
+<template>
+       <div class="projects-cont">
+  <h2 class="sub-title">Projects</h2>
+ </div>
+</template>
+
+<script>
+export default {
+ name: "Projects",
+ mounted() {},
+ data() {
+  return {
+
+  };
+ },
+
+};
+</script>
+
+<style scoped>
+.projects-cont {
+ display: flex;
+ justify-content: center;
+ align-content: center;
+ height: 100vh;
+ width: 100vw;
+}
+</style>
