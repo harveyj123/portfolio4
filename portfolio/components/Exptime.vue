89b035cb3a68ac6c2a7943f25232aea30d769844
Harvey Jiang <harveyjiang11@gmail.com>
2023-09-28 14:59:33 -0400

    quickie

---
+++ portfolio/components/Exptime.vue
@@ -0,0 +1,29 @@
+<template>
+  <li>
+    <div class="flex-start flex items-center pt-3">
+      <div
+        class="-ml-[5px] mr-3 h-[9px] w-[9px] rounded-full bg-neutral-300 dark:bg-neutral-500"></div>
+      <p class="text-sm  ">
+        {{ date }}
+      </p>
+    </div>
+    <div class="mb-6 ml-4 mt-2">
+      <h4 class="mb-1.5 text-xl font-semibold">{{ title }}</h4>
+      <p class="mb-3 ">
+        {{ des }}
+      </p>
+    </div>
+  </li>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  date: String,
+  title: String,
+  des: String,
+})
+</script>
+
+<style scoped>
+
+</style>
--- portfolio/pages/experience.vue
+++ portfolio/pages/experience.vue
@@ -1,13 +1,27 @@
 <template>
-    <div>
-
-    </div>
+  <div class="w-[100vw] h-[100vh] flex justify-center items-center">
+    <ol class="border-l border-neutral-300 dark:border-neutral-500 w-4/5">
+      <Exptime
+        title="University at Buffalo: Research Assistant Under Professor Jinjun Xiong"
+        date="September 2023 - Current"
+        des="Contributed to the development of an application for learning and reading scientific papers in React, implementing
+features like interactive PDF views, data visualizations and UI for thousands of students/professors. Leveraged SQL and fastAPI to model user roles/permissions, retention rate, and assigning readings for students. Collaborated closely with faculty to enhance the user experience and team members to troubleshoot and debug
+issues like routing mishaps, optimizing the application's performance."
+      ></Exptime>
+      <Exptime
+        title="Summer Design Institute: Software Engineer Intern for Staten Island Tech "
+        date="June 2022 - August 2022; June 2023 - August 2023"
+        des="Designed a robust backend for a club attendance app using Express.js, integrating Google's Sheets and Drive APIs to implement a club attendance system. Created a QR code system for fast club attendance taking and REST endpoints for updating attendance and yearly club attendance data instead of a pen and paper attendance system for hundreds of students. Successfully identified and resolved complex issues in a schedule builder app, such as problems with how data is managed and irregularities in the user interface. This ensured a smooth and user-friendly application experience. Assisted students and school staff in rectifying technical issues throughout building. Developed Kiosk software to help connect DOE iPads and aid hundreds of teachers and students in connecting to the WiFi."
+      ></Exptime>
+      <Exptime
+        title="Tech Incubator at Queens College: Intern"
+        date="February 2022 - April 2022"
+        des="Worked with two clients to develop distinct WordPress websites tailored to their specific needs: one for a photography business, focusing on showcasing their portfolio and services, and another for a clothing business, designed to promote their products and brand. Additionally, crafted a WordPress website dedicated to connecting hundreds of current and former interns with freelance job opportunities."
+      ></Exptime>
+    </ol>
+  </div>
 </template>
 
-<script setup lang="ts">
+<script setup lang="ts"></script>
 
-</script>
-
-<style scoped>
-
-</style>
+<style scoped></style>
