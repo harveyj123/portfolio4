acd645f1a857e677d1f2e2d0bb0e5bf9c4c40cfa
Harvey Jiang <71397990+harveyj123@users.noreply.github.com>
2024-01-13 15:44:37 -0500

    portfolio update

--- portfolio/pages/experience.vue
+++ portfolio/pages/experience.vue
@@ -1,17 +1,24 @@
 <template>
-  <div class="w-[100vw] h-[100vh] flex justify-center items-center">
-    <ol class="border-l border-neutral-300 dark:border-neutral-500 w-4/5">
+  <div class="w-[100vw] h-[100vh] flex justify-center items-start">
+    <ol class="border-l border-neutral-300 dark:border-neutral-500 w-4/5 mt-[6rem]">
       <Exptime
         title="University at Buffalo: Research Assistant Under Professor Jinjun Xiong"
         date="September 2023 - Current"
-        des="Contributed to the development of an application for learning and reading scientific papers in React, implementing
-features like interactive PDF views, data visualizations and UI for thousands of students/professors. Leveraged SQL and fastAPI to model user roles/permissions, retention rate, and assigning readings for students. Collaborated closely with faculty to enhance the user experience and team members to troubleshoot and debug
-issues like routing mishaps, optimizing the application's performance."
+        des="Contributed to the development of an application for learning and reading scientific papers in React, implementing features like interactive PDF views and accessible design.
+Developed a interconnected application in Nuxt for professors to assign readings and visualize the data back to them.
+Leveraged mySQL and Django to model user roles/permissions, reading metrics, retention rate, and assigning readings for students.
+Collaborated closely with faculty to enhance the user experience and team members to troubleshoot and debug issues like routing mishaps, optimizing the application's performance.
+"
       ></Exptime>
       <Exptime
         title="Summer Design Institute: Software Engineer Intern for Staten Island Tech "
         date="June 2022 - August 2022; June 2023 - August 2023"
-        des="Designed a robust backend for a club attendance app using Express.js, integrating Google's Sheets and Drive APIs to implement a club attendance system. Created a QR code system for fast club attendance taking and REST endpoints for updating attendance and yearly club attendance data instead of a pen and paper attendance system for hundreds of students. Successfully identified and resolved complex issues in a schedule builder app, such as problems with how data is managed and irregularities in the user interface. This ensured a smooth and user-friendly application experience. Assisted students and school staff in rectifying technical issues throughout building. Developed Kiosk software to help connect DOE iPads and aid hundreds of teachers and students in connecting to the WiFi."
+        des="Designed a robust backend for a club attendance app using Express.js, integrating Google’s Sheets and Drive API to implement a club attendance system.
+Created a QR code system for blazingly fast club attendance taking and rest endpoints for updating attendance and yearly club attendance data instead of a pen-and-paper attendance system for hundreds of students.
+Successfully identified and resolved complex issues in a schedule builder app, such as problems with how data is managed and irregularities in the user interface. This ensured a smooth and user-friendly application experience.
+Assisted students and school staff in rectifying technical issues throughout the building.
+Developed Kiosk software and wrote an API wrapper for the Clearpass API(WiFi) to improve error handling and response/input data safety to help connect DOE iPads and aid hundreds of teachers and students in connecting to the wifi resulting in a smooth and reliable connection experience.
+"
       ></Exptime>
       <Exptime
         title="Tech Incubator at Queens College: Intern"
